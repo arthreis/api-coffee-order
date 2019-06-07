2489c0ee7c2f2123106352e7da55acf5a7d8b35a
arthur.reis <arthur.reis@radixeng.com.br>
2019-06-07 19:47:36 -0300

    add api to doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+
+  const port = normalizePort(process.env.PORT || 3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('DOC-API-COFFEE')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+}
+
+function normalizePort(val){
+  const port = parseInt(val,  10);
+
+  if(isNaN(port)){
+      return val;
+  }
+
+  if(port >= 0){
+      return port;
+  }
+
+  return false;
 }
 bootstrap();
