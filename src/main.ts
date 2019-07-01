ecc07101a88381dfe9e2c767fcf5a1f76bcb4a01
Arthur <arr90rj@gmail.com>
2019-06-30 22:00:32 -0300

    hot reload conf

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import conf from './conf/configuration';
+const config = require('./conf/configuration');
+
+declare const module: any;
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+    app.enableCors();
 
-  const port = normalizePort(process.env.PORT || 3000);
+    const port = normalizePort(process.env.PORT || 3000);
 
-  const options = new DocumentBuilder()
-    .setTitle('DOC-API-COFFEE')
-    .setVersion(conf.getVersion)
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+    const options = new DocumentBuilder()
+        .setTitle('DOC-API-COFFEE')
+        .setVersion(config.version)
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
 
-  console.log("API running on port :"+ port);
+    console.log("API running on port :"+ port);
 
-  await app.listen(port);
+    await app.listen(port);
+
+    if(module.hot){
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 
 function normalizePort(val){
-  const port = parseInt(val,  10);
+    const port = parseInt(val,  10);
 
-  if(isNaN(port)){
-      return val;
-  }
+    if(isNaN(port)){
+        return val;
+    }
 
-  if(port >= 0){
-      return port;
-  }
+    if(port >= 0){
+        return port;
+    }
 
-  return false;
+    return false;
 }
 bootstrap();
