386c123d553de394b5045e8a285ab2ff317b073f
Arthur <arr90rj@gmail.com>
2019-07-17 02:38:51 -0300

    WIP -alteracao conf docker-mongo; -add versao node que roda a api; -add variavel de ambiente para string de conexao do banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,12 @@ import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
 import { ServiceOrdersModule } from './service-orders/service-orders.module';
 
-const DEV = "mongodb://coffee-order-user:c0ff33-0rd3r@ds261296.mlab.com:61296/coffee-order";
+const DEV = process.env.DATABASE_URL;
 const LOCAL  = "mongodb://0.0.0.0/coffee-order";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(DEV, {useNewUrlParser: true}),
+    MongooseModule.forRoot(DEV || LOCAL, {useNewUrlParser: true}),
     CoffeeModule,
     UsersModule,
     OrdersModule,
